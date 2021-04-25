78f11d5b5a6cc6397162bc4672c2b83f792c2384
stefan goebel <goebel.st@gmail.com>
2021-04-25 22:38:37 +0200

    implement resource

--- src/main/java/com/qm/example/jaxrs/endpoint/IceCreamResource.java
+++ src/main/java/com/qm/example/jaxrs/endpoint/IceCreamResource.java
@@ -3,8 +3,12 @@ package com.qm.example.jaxrs.endpoint;
 import org.openapitools.api.IcecreamsApi;
 import org.openapitools.model.RestIceCreamResponse;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Path;
 
+@ApplicationScoped
+@Path("")
 public class IceCreamResource implements IcecreamsApi {
 
     @Inject
